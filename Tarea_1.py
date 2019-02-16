662f0ce9a3cfe756d00e6f854b761600577b7653
albertorubi <46937565+albertorubi@users.noreply.github.com>
2019-02-16 11:44:34 -0600

    Add files via upload

---
+++ Tarea_1.py
@@ -0,0 +1,50 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+import math
+
+
+# In[6]:
+
+
+stringValue = '******'
+
+
+# In[18]:
+
+
+print("%.1s %.7f" % ( stringValue, math.pi))
+
+
+# In[19]:
+
+
+print("%.2s %.6f" % ( stringValue, math.pi))
+
+
+# In[20]:
+
+
+print("%.3s %.5f" % ( stringValue, math.pi))
+
+
+# In[21]:
+
+
+print("%.4s %.4f" % ( stringValue, math.pi))
+
+
+# In[22]:
+
+
+print("%.5s %.3f" % ( stringValue, math.pi))
+
+
+# In[23]:
+
+
+print("%.6s %.2f" % ( stringValue, math.pi))
+
